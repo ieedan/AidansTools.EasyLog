2960b3f68f22fbfb517a26e48521498ffadc2a04
Aidan Bleser <aidanbleser35@gmail.com>
2022-11-07 16:52:05 -0600

    Sub Folders
    Added ability to specify a sub directory to create the logs to such as \Logs\Logs.txt

---
+++ AidansTools.EasyLog/EasyLog.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace AidansTools.Logging
+{
+    /// <summary>
+    /// Allows you to easily log data with built in events for errors
+    /// </summary>
+    public class EasyLog
+    {
+        private string FilePath;
+        /// <summary>
+        /// Creates new instance of easy log named as 'AidansTools.EasyLog'
+        /// </summary>
+        public EasyLog()
+        {
+            FilePath = AppDomain.CurrentDomain.BaseDirectory + $"AidansTools.EasyLog.txt";
+        }
+
+        /// <summary>
+        /// Specify the name of your log
+        /// </summary>
+        /// <param name="name"></param>
+        public EasyLog(string name)
+        {
+            FilePath = AppDomain.CurrentDomain.BaseDirectory + $"{name}.txt";
+        }
+
+        /// <summary>
+        /// Allows you to add a name of a folder to store the log in no need to include backslashes
+        /// 
+        /// <example>
+        ///     \Logs\Logs.txt
+        /// </example>
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="fileInDirectory"></param>
+        public EasyLog(string name, string fileInDirectory)
+        {
+            string fullPath = AppDomain.CurrentDomain.BaseDirectory + fileInDirectory;
+            if (!Directory.Exists(fullPath))
+            {
+                Directory.CreateDirectory(fullPath);
+            }
+            FilePath = fullPath + @"\" + $"{name}.txt";
+        }
+
+        /// <summary>
+        /// Logs info with date-time stamp to FilePath specified from the constuctor
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public Task LogInfo(string text)
+        {
+            string fulltext = $"[{DateTime.Now}] {text}";
+
+            File.AppendAllText(FilePath, fulltext + Environment.NewLine);
+
+            System.Diagnostics.Debug.WriteLine(fulltext);
+
+            NewInfoLogged?.Invoke();
+
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Logs info with date-time stamp to FilePath specified from the constuctor in all caps to be identified as an error
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public Task LogError(string text, [CallerMemberName] string name = null)
+        {
+            string fulltext = $"[{DateTime.Now}] [Called by: {name}] {text}".ToUpper();
+
+            File.AppendAllText(FilePath, fulltext + Environment.NewLine);
+
+            System.Diagnostics.Debug.WriteLine(fulltext);
+
+            NewErrorLogged?.Invoke(0);
+
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Logs info with date-time stamp to FilePath specified from the constuctor in all caps to be identified as an error allows for error code
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        public Task LogError(string text, int code, [CallerMemberName] string name = null)
+        {
+            string fulltext = $"[{DateTime.Now}] [Error Code {code}] [Called by: {name}] {text}".ToUpper();
+
+            File.AppendAllText(FilePath, fulltext + Environment.NewLine);
+
+            System.Diagnostics.Debug.WriteLine(fulltext);
+
+            NewErrorLogged?.Invoke(code);
+
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Clears the logs from the specified file
+        /// </summary>
+        /// <returns></returns>
+        public Task FlushLog()
+        {
+            File.WriteAllText(FilePath, String.Empty);
+
+            System.Diagnostics.Debug.WriteLine($"Clearing logs in {FilePath}");
+
+            LogCleared?.Invoke();
+
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Fires when logs are cleared
+        /// </summary>
+        public event Action LogCleared;
+
+        /// <summary>
+        /// Fires when new info is logged
+        /// </summary>
+        public event Action NewInfoLogged;
+
+        /// <summary>
+        /// Will fire when error is logged and will return an error code if included
+        /// </summary>
+        public event Action<int> NewErrorLogged;
+    }
+}
